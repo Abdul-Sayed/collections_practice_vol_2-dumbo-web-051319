9c86ce21c71f137f479f670dfe749de2d3f7ab4e
Abdul <awsayed@alaska.edu>
2019-05-05 22:09:40 +0000

    Automatically backed up by Learn

--- spec/collections_practice_spec.rb
+++ spec/collections_practice_spec.rb
@@ -2,56 +2,56 @@ require 'spec_helper'
 
 describe 'collections practice vol 2.' do
 
-  let(:keys) {
-    [
-        {
-            :first_name => "blake",
-                 :motto => "Have a koala-ty day!"
-        },
-        {
-            :first_name => "ashley",
-                 :motto => "I dub thee, 'Lady Brett Ashley'."
-        }
-    ]
-  }
+  # let(:keys) {
+  #   [
+  #       {
+  #           :first_name => "blake",
+  #               :motto => "Have a koala-ty day!"
+  #       },
+  #       {
+  #           :first_name => "ashley",
+  #               :motto => "I dub thee, 'Lady Brett Ashley'."
+  #       }
+  #   ]
+  # }
 
-  let(:data) {
-    [
-           {
-             "blake" => {
-                :awesomeness => 10,
-                     :height => "74",
-                  :last_name => "johnson"
-            },
-            "ashley" => {
-                :awesomeness => 9,
-                     :height => 60,
-                  :last_name => "dubs"
-            }
-        }
-    ]
-  }
+  # let(:data) {
+  #   [
+  #         {
+  #           "blake" => {
+  #               :awesomeness => 10,
+  #                   :height => "74",
+  #                 :last_name => "johnson"
+  #           },
+  #           "ashley" => {
+  #               :awesomeness => 9,
+  #                   :height => 60,
+  #                 :last_name => "dubs"
+  #           }
+  #       }
+  #   ]
+  # }
 
-  let(:merged_data) {
-    [
-        {
-             :first_name => "blake",
-            :awesomeness => 10,
-                 :height => "74",
-              :last_name => "johnson",
-                  :motto => "Have a koala-ty day!"
+  # let(:merged_data) {
+  #   [
+  #       {
+  #           :first_name => "blake",
+  #           :awesomeness => 10,
+  #               :height => "74",
+  #             :last_name => "johnson",
+  #                 :motto => "Have a koala-ty day!"
 
-        },
-        {
-             :first_name => "ashley",
-            :awesomeness => 9,
-                 :height => 60,
-              :last_name => "dubs",
-                  :motto => "I dub thee, 'Lady Brett Ashley'."
+  #       },
+  #       {
+  #           :first_name => "ashley",
+  #           :awesomeness => 9,
+  #               :height => 60,
+  #             :last_name => "dubs",
+  #                 :motto => "I dub thee, 'Lady Brett Ashley'."
 
-        }
-    ]
-  }
+  #       }
+  #   ]
+  # }
 
   let(:cool) {
     [
