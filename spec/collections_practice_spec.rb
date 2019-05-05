6201f2c5a6d3d2a77cfda630ab414b7c013fc81a
Abdul <awsayed@alaska.edu>
2019-05-05 22:06:36 +0000

    Automatically backed up by Learn

--- spec/collections_practice_spec.rb
+++ spec/collections_practice_spec.rb
@@ -13,7 +13,18 @@ describe 'collections practice vol 2.' do
 	}
 ]
 
-  let(:data) 
+  let(:data) {
+	"blake" => {
+		:awesomeness => 10,
+		:height => "74",
+		:last_name => "johnson"
+	},
+	"ashley" => {
+		:awesomeness => 9,
+		:height => 60,
+		:last_name => "dubs"
+	}
+}
 
   let(:merged_data) {
     [
